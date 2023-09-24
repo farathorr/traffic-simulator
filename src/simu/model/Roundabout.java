24aface1d8869d50d7b618b0713254a8d23bed94
Aapo Salminen <aapo.sirius.salminen9@gmail.com>
2023-09-24 20:26:31 +0300

    Create Roundabout.java

---
+++ src/simu/model/Roundabout.java
@@ -0,0 +1,14 @@
+package simu.model;
+
+import eduni.distributions.ContinuousGenerator;
+import simu.framework.EventList;
+
+public class Roundabout extends ServicePoint{
+
+
+    public Roundabout(ContinuousGenerator generator, EventList eventList, EventType tyyppi) {
+        super(generator, eventList, tyyppi);
+    }
+
+
+}
