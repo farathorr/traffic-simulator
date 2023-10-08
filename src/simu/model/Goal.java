296c9ed1ef190e2b3fc2955d1db128b905924e36
Aapo Salminen <aapo.sirius.salminen9@gmail.com>
2023-10-08 17:19:31 +0300

    Add level 5 and goal class

---
+++ src/simu/model/Goal.java
@@ -0,0 +1,26 @@
+package simu.model;
+
+import eduni.distributions.ContinuousGenerator;
+import simu.framework.Clock;
+import simu.framework.Event;
+import simu.framework.EventList;
+
+public class Goal extends ServicePoint {
+    public Goal(ContinuousGenerator generator, EventList eventList, String type) {
+        super(generator, eventList, type);
+    }
+
+    @Override
+    public void startService() {  //Aloitetaan uusi palvelu, asiakas on jonossa palvelun aikana
+        reserved = true;
+        queue.peek().setFirstInQueue(true);
+        eventList.add(new Event(scheduledEventType, Clock.getInstance().getTime() + 2));
+    }
+
+    @Override
+    public void addToQueue(Customer customer) {
+        queue.add(customer);
+        customer.addDestination(this.getX() + (Math.random() - 0.5), this.getY() + (Math.random() - 0.5));
+        customer.setFirstInQueue(false);
+    }
+}
