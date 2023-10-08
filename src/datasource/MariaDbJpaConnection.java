1f0072749e9b35c0a2d5a9e07cec825ebd88df02
Nasim Zouneibiri <111856757+nasudesu@users.noreply.github.com>
2023-10-08 15:23:33 +0300

    Add meta-inf

---
+++ src/datasource/MariaDbJpaConnection.java
@@ -0,0 +1,23 @@
+package datasource;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class MariaDbJpaConnection {
+
+    private static EntityManagerFactory emf = null;
+    private static EntityManager em = null;
+
+    public static EntityManager getInstance() {
+        // you need to add synchronization if you run in a multi-threaded environment
+
+        if (em==null) {
+            if (emf==null) {
+                emf = Persistence.createEntityManagerFactory("datasource");
+            }
+            em = emf.createEntityManager();
+        }
+        return em;
+    }
+}
---
+++ src/simu/model/Simulation.java
@@ -0,0 +1,4 @@
+package simu.model;
+
+public class Simulation {
+}
