a303e8e9ff7e0e454b56b0a79a45ff66d6f8927d
Nasim Zouneibiri <111856757+nasudesu@users.noreply.github.com>
2023-10-14 16:36:49 +0300

    Jaca doc

--- src/simu/dao/ResultsDao.java
+++ src/simu/dao/ResultsDao.java
@@ -7,8 +7,15 @@ import simu.entity.Results;
 
 import java.util.List;
 
+/**
+ * Tämä luokka on Results luokkaa varten tehty Dao ja tässä luokassa on metodeja joilla tallennetaan
+ * Results luokan olioita titokantaan sekä haetaan titokannasta.
+ */
 public class ResultsDao {
 
+    /**
+     * @param results on Results luokan olio joka tallennetaan tietokantaan.
+     */
     public void persist(Results results) {
         EntityManager em = MariaDbJpaConnection.getInstance();
         em.getTransaction().begin();
@@ -16,6 +23,10 @@ public class ResultsDao {
         em.getTransaction().commit();
     }
 
+    /**
+     * @param results on Results luokan olio joka päivitetään tietokantaan.
+     * @return
+     */
     public Results update(Results results) {
         EntityManager em = MariaDbJpaConnection.getInstance();
         em.getTransaction().begin();
@@ -24,6 +35,9 @@ public class ResultsDao {
         return results;
     }
 
+    /**
+     * @param results on Results luokan olio joka poistetaan tietokannasta.
+     */
     public void delete(Results results) {
         EntityManager em = MariaDbJpaConnection.getInstance();
         em.getTransaction().begin();
@@ -31,6 +45,9 @@ public class ResultsDao {
         em.getTransaction().commit();
     }
 
+    /**
+     * @return palauttaa listan kaikista Results olioista.
+     */
     public List<Results> getAll() {
         EntityManager em = MariaDbJpaConnection.getInstance();
         TypedQuery<Results> query = em.createQuery("SELECT c FROM Results c", Results.class);
@@ -38,6 +55,10 @@ public class ResultsDao {
         return results;
     }
 
+    /**
+     * @param id Etsii tietokannasta Results olion id:n perusteella.
+     * @return
+     */
     public Results find(int id) {
         EntityManager em = MariaDbJpaConnection.getInstance();
         Results results = em.find(Results.class, id);
