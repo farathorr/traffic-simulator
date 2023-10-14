1378a379bd717a9bb4b63305cabcfcd59c641a97
Aapo Salminen <aapo.sirius.salminen9@gmail.com>
2023-10-14 16:37:52 +0300

    Update Trace.java

--- src/simu/framework/Trace.java
+++ src/simu/framework/Trace.java
@@ -1,6 +1,13 @@
 package simu.framework;
 
+/**
+ * Trace-luokka, joka sisältää enumeja ja metodeja konsoliin tulevia printtejä varten.
+ */
 public class Trace {
+	/**
+	 * Level enum, joka sisältää eri tasoja.
+	 * Level.INFO 
+	 */
 	public enum Level{INFO, WAR, ERR}
 	private static Level traceLevel = Level.INFO;
 	public static void setTraceLevel(Level lvl) {
