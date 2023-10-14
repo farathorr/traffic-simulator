48b9dcffb4da34f9ab6f6751c71f278bb3219686
Aapo Salminen <aapo.sirius.salminen9@gmail.com>
2023-10-14 14:25:41 +0300

    Add test directory

---
+++ src/test/java/simu/model/ServicePointTest.java
@@ -0,0 +1,13 @@
+package simu.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServicePointTest {
+
+    @Test
+    void takeFromQueue() {
+
+    }
+}
